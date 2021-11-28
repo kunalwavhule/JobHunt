fe08b7d0bbe682c0ecd70eb7132f23a0528824d2
kunal wahule <Kunalskw143@gmail.com>
2021-11-28 23:23:18 +0530

    splash screen

---
+++ app/src/main/java/com/example/jobhunt/SplachScreen.java
@@ -0,0 +1,25 @@
+package com.example.jobhunt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplachScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splach_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },3000);
+
+    }
+}
