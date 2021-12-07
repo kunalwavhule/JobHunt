a32a035fd0f8663c642898ed2378f1821f495852
kunal wahule <Kunalskw143@gmail.com>
2021-12-07 18:10:40 +0530

    Login out

--- app/src/main/java/com/example/jobhunt/MainActivity.java
+++ app/src/main/java/com/example/jobhunt/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.jobhunt;
 
 import  androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,4 +18,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void Logout(View view) {
+        auth.signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+    }
 }
