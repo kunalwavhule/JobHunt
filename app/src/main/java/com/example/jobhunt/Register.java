d0d2eed8ee828649dacff99ab62f1de6edb11cd9
kunal wahule <Kunalskw143@gmail.com>
2021-12-07 19:11:16 +0530

    Login in

---
+++ app/src/main/java/com/example/jobhunt/Register.java
@@ -0,0 +1,21 @@
+package com.example.jobhunt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void Direct2(View view) {
+    }
+
+    public void Register(View view) {
+    }
+}
