fdf6bdf60878b6d7bb126ad595ae55d54e5d6d29
kunal wahule <Kunalskw143@gmail.com>
2021-12-10 03:14:46 +0530

    Post Job

---
+++ app/src/main/java/com/example/jobhunt/InsertJobPostActivity.java
@@ -0,0 +1,14 @@
+package com.example.jobhunt;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InsertJobPostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_job_post);
+    }
+}
--- app/src/main/java/com/example/jobhunt/PostJobActivity.java
+++ app/src/main/java/com/example/jobhunt/PostJobActivity.java
@@ -1,14 +1,27 @@
 package com.example.jobhunt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class PostJobActivity extends AppCompatActivity {
 
+    FloatingActionButton fabBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_job);
+        fabBtn = findViewById(R.id.fab_add);
+        fabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),InsertJobPostActivity.class));
+
+            }
+        });
     }
 }
