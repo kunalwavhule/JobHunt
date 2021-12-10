983aad3da752aa757e82a180ca9055ef132c2635
kunal wahule <Kunalskw143@gmail.com>
2021-12-10 14:42:30 +0530

    Post Job

---
+++ app/src/main/java/com/example/jobhunt/Model/Data.java
@@ -0,0 +1,67 @@
+package com.example.jobhunt.Model;
+
+public class Data {
+    String title;
+    String description;
+    String skill;
+    String salary;
+    String id;
+    String date;
+
+    public Data(String title, String description, String skill, String salary, String id, String date) {
+        this.title = title;
+        this.description = description;
+        this.skill = skill;
+        this.salary = salary;
+        this.id = id;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
