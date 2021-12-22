98af9ce6e9b09212db0de738ca674e9d1c48496e
kunal wahule <Kunalskw143@gmail.com>
2021-12-22 23:53:28 +0530

    admin dashboard

--- app/src/main/java/com/example/jobhunt/MainActivity.java
+++ app/src/main/java/com/example/jobhunt/MainActivity.java
@@ -2,24 +2,13 @@ package com.example.jobhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-
-import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    FirebaseAuth auth = FirebaseAuth.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void Logout(View view) {
-
-        auth.signOut();
-        startActivity(new Intent(getApplicationContext(),Login.class));
-        finish();
     }
 }
---
+++ app/src/main/java/com/example/jobhunt/RecruiterDashboard.java
@@ -0,0 +1,112 @@
+package com.example.jobhunt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
+
+import com.example.jobhunt.Model.PostJobData;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RecruiterDashboard extends AppCompatActivity {
+    FloatingActionButton fabBtn;
+    // recycler view
+    RecyclerView recyclerView;
+    PostJobAdapter postJobAdapter;
+    FirebaseAuth auth;
+    private DatabaseReference mJobPost;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recruiter_dashboard);
+        fabBtn = findViewById(R.id.fab_add);
+        recyclerView = findViewById(R.id.recycler_job_post_id);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = auth.getCurrentUser();
+        String uid = mUser.getUid();
+
+
+
+
+        mJobPost = FirebaseDatabase.getInstance().getReference().child("Job Post").child(uid);
+
+        FirebaseRecyclerOptions<PostJobData> options =
+                new FirebaseRecyclerOptions.Builder<PostJobData>()
+                        .setQuery(mJobPost, PostJobData.class)
+                        .build();
+
+        postJobAdapter = new PostJobAdapter(options);
+        recyclerView.setAdapter(postJobAdapter);
+
+
+        // recyclerview
+
+
+        fabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),InsertJobPostActivity.class));
+
+            }
+        });
+
+
+    }
+
+    public void Logout(View view) {
+        auth.signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        postJobAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        postJobAdapter.stopListening();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search,menu);
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+}
