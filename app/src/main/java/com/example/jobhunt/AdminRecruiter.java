76892a2d4f0ce0a78a4022a4139b9475b5b8ad77
kunal wahule <Kunalskw143@gmail.com>
2021-12-23 18:32:52 +0530

    dashboard

--- app/src/main/java/com/example/jobhunt/AdminRecruiter.java
+++ app/src/main/java/com/example/jobhunt/AdminRecruiter.java
@@ -1,14 +1,100 @@
 package com.example.jobhunt;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+
+import com.example.jobhunt.Model.Data;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class AdminRecruiter extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ApplicantAdapter applicantAdapter;
+    FirebaseAuth auth;
+    private DatabaseReference mJobPost;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_recruiter);
+
+        recyclerView = findViewById(R.id.rvar);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser mUser = auth.getCurrentUser();
+        String uid = mUser.getUid();
+
+        mJobPost = FirebaseDatabase.getInstance().getReference().child("Recruiter");
+
+        FirebaseRecyclerOptions<Data> options =
+                new FirebaseRecyclerOptions.Builder<Data>()
+                        .setQuery(mJobPost, Data.class)
+                        .build();
+
+        applicantAdapter = new ApplicantAdapter(options);
+        recyclerView.setAdapter(applicantAdapter);
+
+
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        applicantAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        applicantAdapter.stopListening();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search,menu);
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                txtSearch(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                txtSearch(s);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void txtSearch(String str){
+
+        FirebaseRecyclerOptions<Data> options =
+                new FirebaseRecyclerOptions.Builder<Data>()
+                        .setQuery(mJobPost.orderByChild("fullname").startAt(str).endAt(str+"~"), Data.class)
+                        .build();
+
+        applicantAdapter = new ApplicantAdapter(options);
+        applicantAdapter.startListening();
+        recyclerView.setAdapter(applicantAdapter);
+
     }
 }
--- app/src/main/java/com/example/jobhunt/Register.java
+++ app/src/main/java/com/example/jobhunt/Register.java
@@ -98,8 +98,18 @@ public class Register extends AppCompatActivity {
                 String id = authResult.getUser().getUid();
 
                 Data data = new Data(id,rname,remail,rpassword,rphoneno,date,types);
-                firebaseDatabase.getReference().child("Applicant").child(id).setValue(data);
-                Toast.makeText(Register.this,"Register successfully",Toast.LENGTH_LONG).show();
+                if(types == 0){
+                    firebaseDatabase.getReference().child("Applicant").child(id).setValue(data);
+                    Toast.makeText(Register.this,"Register successfully",Toast.LENGTH_LONG).show();
+
+                }
+                if (types == 1){
+                    firebaseDatabase.getReference().child("Recruiter").child(id).setValue(data);
+                    Toast.makeText(Register.this,"Register successfully",Toast.LENGTH_LONG).show();
+
+                }
+             //   firebaseDatabase.getReference().child("Applicant").child(id).setValue(data);
+              //  Toast.makeText(Register.this,"Register successfully",Toast.LENGTH_LONG).show();
                 startActivity(new Intent(getApplicationContext(),Login.class));
 
 
