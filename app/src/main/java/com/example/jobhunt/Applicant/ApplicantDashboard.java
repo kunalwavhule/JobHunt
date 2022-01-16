07cea63918d3bf782953130e047ef592b75043b0
kunal wahule <Kunalskw143@gmail.com>
2022-01-17 04:44:05 +0530

    Final commit

--- app/src/main/java/com/example/jobhunt/Applicant/ApplicantDashboard.java
+++ app/src/main/java/com/example/jobhunt/Applicant/ApplicantDashboard.java
@@ -7,7 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -41,6 +41,7 @@ public class ApplicantDashboard extends AppCompatActivity {
     String phone_no,name,email,applicanteducationdesc,
             applicantjobrole,applicantcompany,
             applicantjobdescription,applicantexperience;
+    FirebaseRecyclerAdapter<PostJobData, myViewHolder> firebaseRecyclerAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,7 +57,7 @@ public class ApplicantDashboard extends AppCompatActivity {
                         .setQuery(mJob, PostJobData.class)
                         .build();
 
-        FirebaseRecyclerAdapter<PostJobData, myViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<PostJobData, myViewHolder>(options) {
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<PostJobData, myViewHolder>(options) {
             @Override
             protected void onBindViewHolder(@NonNull myViewHolder holder, int position, @NonNull PostJobData model) {
                 holder.title.setText(model.getTitle());
@@ -149,7 +150,7 @@ public class ApplicantDashboard extends AppCompatActivity {
                         FirebaseDatabase.getInstance().getReference().child("Applied Status").child(auth.getUid()).child(getRef(position).getKey()).updateChildren(map).addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void unused) {
-                                Toast.makeText(holder.title.getContext(),"data is updated",Toast.LENGTH_LONG).show();
+                                Toast.makeText(holder.title.getContext(),"Successfully Applied "+model.getTitle(),Toast.LENGTH_LONG).show();
                             }
                         }).addOnFailureListener(new OnFailureListener() {
                             @Override
@@ -161,7 +162,7 @@ public class ApplicantDashboard extends AppCompatActivity {
                         FirebaseDatabase.getInstance().getReference("Applied").child(model.getId()).child(auth.getUid()).updateChildren(rap).addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void unused) {
-                                Toast.makeText(holder.title.getContext(),"Update data on Firebase Save",Toast.LENGTH_LONG).show();
+                                Toast.makeText(holder.title.getContext(),"Successfully Applied "+model.getTitle(),Toast.LENGTH_LONG).show();
                             }
                         });
                     }
@@ -177,7 +178,8 @@ public class ApplicantDashboard extends AppCompatActivity {
             }
         } ;
         firebaseRecyclerAdapter.startListening();
-        recyclerView.setAdapter(firebaseRecyclerAdapter);
+       recyclerView.setAdapter(firebaseRecyclerAdapter);
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setSelectedItemId(R.id.home);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -203,19 +205,169 @@ public class ApplicantDashboard extends AppCompatActivity {
             }
         });
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.logout,menu);
+        getMenuInflater().inflate(R.menu.search,menu);
+        getMenuInflater().inflate(R.menu.logout, menu);
         MenuItem logout = menu.findItem(R.id.lagout);
         logout.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
-            public boolean onMenuItemClick(MenuItem menuItem) {
+            public boolean onMenuItemClick(MenuItem item) {
                 auth.signOut();
                 startActivity(new Intent(getApplicationContext(), Login.class));
                 finish();
                 return false;
             }
         });
+        MenuItem item = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                txtSearch(s);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                txtSearch(s);
+                return false;
+            }
+        });
         return super.onCreateOptionsMenu(menu);
+
     }
-}
+
+    private void txtSearch(String str){
+
+        FirebaseRecyclerOptions<PostJobData> options =
+                new FirebaseRecyclerOptions.Builder<PostJobData>()
+                        .setQuery(mJob.orderByChild("title").startAt(str).endAt(str+"~"), PostJobData.class)
+                        .build();
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<PostJobData, myViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull myViewHolder holder, int position, @NonNull PostJobData model) {
+                holder.title.setText(model.getTitle());
+                holder.description.setText(model.getDescription());
+                holder.skill.setText(model.getSkill());
+                holder.salary.setText(model.getSalary());
+                holder.date.setText("posted job :\t"+model.getDate());
+                holder.city.setText(model.getCity());
+                holder.jobtypes.setText(model.getJobtypes());
+                holder.schedule.setText(model.getSchedule());
+                holder.company.setText(model.getCompany());
+
+                mDatabase = FirebaseDatabase.getInstance().getReference();
+                ValueEventListener postListener = new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        name = snapshot.child("User").child(FirebaseAuth.getInstance().getUid()).child("fullname").getValue(String.class);
+                        phone_no = snapshot.child("User").child(FirebaseAuth.getInstance().getUid()).child("phoneno").getValue(String.class);
+                        email = snapshot.child("User").child(FirebaseAuth.getInstance().getUid()).child("email").getValue(String.class);
+
+                        applicantcompany = snapshot.child("User").child(FirebaseAuth.getInstance().getUid()).child("applicantcompany").getValue(String.class);
+                        applicantjobrole = snapshot.child("User").child(FirebaseAuth.getInstance().getUid()).child("applicantjobrole").getValue(String.class);
+                        applicantjobdescription = snapshot.child("User").child(FirebaseAuth.getInstance().getUid()).child("applicantjobdescription").getValue(String.class);
+                        applicantexperience = snapshot.child("User").child(FirebaseAuth.getInstance().getUid()).child("applicantexperience").getValue(String.class);
+
+                        applicanteducationdesc = snapshot.child("User").child(FirebaseAuth.getInstance().getUid()).child("applicanteducationdesc").getValue(String.class);
+
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                    }
+                };
+                mDatabase.addValueEventListener(postListener);
+
+                //Save Button
+                holder.btnSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Map<String,Object> map = new HashMap<>();
+                        map.put(auth.getUid(),"1");
+
+                        FirebaseDatabase.getInstance().getReference().child("Job Post").child(getRef(position).getKey()).updateChildren(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(holder.title.getContext(),"Job Saved Suceessfully"+model.getTitle(),Toast.LENGTH_LONG).show();
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(holder.title.getContext(),"Error while updating",Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                });
+                holder.btnApply.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Map<String,Object> map = new HashMap<>();
+                        map.put("skill",model.getSkill());
+                        map.put("city",model.getCity());
+                        map.put("company",model.getCompany());
+                        map.put("date",model.getDate());
+                        map.put("salary",model.getSalary());
+                        map.put("skill",model.getSkill());
+                        map.put("title",model.getTitle());
+                        map.put("description",model.getDescription());
+                        map.put("status","processing");
+                        map.put("id",auth.getUid());
+                        Map<String,Object> rap = new HashMap<>();
+                        rap.put("skill",model.getSkill());
+                        rap.put("city",model.getCity());
+                        rap.put("company",model.getCompany());
+                        rap.put("date",model.getDate());
+                        rap.put("salary",model.getSalary());
+                        rap.put("skill",model.getSkill());
+                        rap.put("title",model.getTitle());
+                        rap.put("description",model.getDescription());
+
+                        rap.put("fullname",name);
+                        rap.put("phoneno",phone_no);
+                        rap.put("email",email);
+                        rap.put("applicantjobrole",applicantjobrole);
+                        rap.put("applicantcompany", applicantcompany);
+                        rap.put("applicantjobdescription",applicantjobdescription);
+                        rap.put("applicantexperience",applicantexperience);
+                        rap.put("applicanteducationdesc",applicanteducationdesc);
+                        rap.put("uid",auth.getUid());
+                        rap.put("pushid",getRef(position).getKey());
+                        FirebaseDatabase.getInstance().getReference().child("Applied Status").child(auth.getUid()).child(getRef(position).getKey()).updateChildren(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(holder.title.getContext(),"Successfully Applied "+model.getTitle(),Toast.LENGTH_LONG).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(holder.title.getContext(),"Error while updating",Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+                        FirebaseDatabase.getInstance().getReference("Applied").child(model.getId()).child(auth.getUid()).updateChildren(rap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(holder.title.getContext(),"Successfully Applied "+model.getTitle(),Toast.LENGTH_LONG).show();
+                            }
+                        });
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.applicant_post_job_item,parent,false);
+                return new myViewHolder(view);
+
+            }
+        } ;
+        firebaseRecyclerAdapter.startListening();
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
+
+    }
+  }
--- app/src/main/java/com/example/jobhunt/Recruiter/RecruiterDashboard.java
+++ app/src/main/java/com/example/jobhunt/Recruiter/RecruiterDashboard.java
@@ -135,6 +135,4 @@ public class RecruiterDashboard extends AppCompatActivity {
         recyclerView.setAdapter(postJobAdapter);
 
     }
-
-
 }
